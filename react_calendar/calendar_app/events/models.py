5356ac423ebc6005b61d347c627189938a160dd3
Inferato <infaustmager@gmail.com>
2024-07-04 20:00:23 +0300

    feat: test

--- react_calendar/calendar_app/events/models.py
+++ react_calendar/calendar_app/events/models.py
@@ -5,6 +5,7 @@ class Event(models.Model):
     description = models.TextField()
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
